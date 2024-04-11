6880fa8f3024691a081fb071419a09ecdb058f62
yarkin-05 <125620095+yarkin-05@users.noreply.github.com>
2024-04-11 08:54:18 -0600

    added auth and fire services

--- super/src/app/services/auth.service.ts
+++ super/src/app/services/auth.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail
+} from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth:Auth) {}
+
+  async loginUser(email: string, password: string) {
+    try {
+      const user = await signInWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async registerUser(email: string, password: string) {
+    try {
+      const user = await createUserWithEmailAndPassword(this.auth, email, password);
+      return user;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+  async logout() {
+    try {
+      await signOut(this.auth);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async resetPassword(email: string) {
+    try {
+      await sendPasswordResetEmail(this.auth, email);
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
 }
+  
+
