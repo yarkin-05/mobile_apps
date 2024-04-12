349c4094aa3786d20847c5a7abd6cff8b5f88949
yarkin-05 <125620095+yarkin-05@users.noreply.github.com>
2024-04-11 19:20:55 -0600

    added auth and fire services

--- super/src/app/sign-up/sign-up.page.ts
+++ super/src/app/sign-up/sign-up.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router, private alertCtrl: AlertController) { }
+
 
   ngOnInit() {
   }
 
+  async signUp(form): Promise<void> {
+    console.log('registration ok');
+    this.authService.loginUser(form.value.email, form.value.password).then(
+      () => {
+        this.router.navigateByUrl('home');
+      },
+      async error => {
+        const alert = await this.alertCtrl.create({
+          message: error.message,
+          buttons: [{ text: 'Ok', role: 'cancel' }],
+        });
+        await alert.present();
+      }
+    )
+  }
 }
