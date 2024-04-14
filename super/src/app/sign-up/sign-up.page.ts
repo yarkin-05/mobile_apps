3487cdc1eae94605361105b6e3ebb748c4e8a091
yarkin-05 <125620095+yarkin-05@users.noreply.github.com>
2024-04-13 22:39:06 -0600

    Update sign-up.page.ts

--- super/src/app/sign-up/sign-up.page.ts
+++ super/src/app/sign-up/sign-up.page.ts
@@ -6,7 +6,7 @@ import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
-  styleUrls: ['./sign-up.page.scss'],
+  styleUrls: './sign-up.page.scss'],
 })
 export class SignUpPage implements OnInit {
 
